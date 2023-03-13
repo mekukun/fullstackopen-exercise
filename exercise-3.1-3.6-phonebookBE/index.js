6dfaf0b4c0e6f684022bccf290072513ef7692b6
meku <piangree147@gmail.com>
2023-03-14 00:48:05 +0800

    Exercises 3.15.-3.18
    - Adding error handling middleware

--- exercise-3.1-3.6-phonebookBE/index.js
+++ exercise-3.1-3.6-phonebookBE/index.js
@@ -95,6 +95,28 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+// Error handling middlewares
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
