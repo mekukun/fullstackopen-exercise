6be9c4459126caac0fc5d6c44e8f289003c6edfd
meku <piangree147@gmail.com>
2023-03-16 23:10:37 +0800

    Exercises 3.19
    - Add validation of input data when update.

--- exercise-3.1-3.6-phonebookBE/index.js
+++ exercise-3.1-3.6-phonebookBE/index.js
@@ -63,14 +63,13 @@ app.get("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -87,7 +86,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((err) => next(err));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -107,9 +106,12 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((person) => {
-    response.json(person);
-  });
+  person
+    .save()
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 // Error handling middlewares
@@ -126,6 +128,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
